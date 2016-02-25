29470a4b9e847525a289372004495014b7c0e4e7
yuer3385 <renwei_home@126.com>
2016-02-25 20:22:41 +0800

    add user destroy action

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219071333) do
+ActiveRecord::Schema.define(version: 20160225111046) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content",    limit: 65535
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160219071333) do
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
     t.string   "remember_digest", limit: 255
+    t.boolean  "admin",           limit: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
