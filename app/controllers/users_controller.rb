e85e0fb0b87ee866bf0186eba35f93b03caa896f
yuer3385 <renwei_home@126.com>
2016-03-13 17:14:19 +0800

    save changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # GET /users/new
